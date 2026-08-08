2b6962bacf182e5f638815485a24025bfc1846b2
minami73 <jj.c.r.1781@gmail.com>
2026-08-07 22:22:48 -0600

    Switch comenzado

--- programa-101/app.ts
+++ programa-101/app.ts
@@ -1,1 +1,15 @@
-console.log('Holis xD');
+let weekDay: number = 4;
+
+switch(weekDay){
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+    case 3:
+        console.log('Miércoles');
+        break;
+    default:
+        console.log('No es ni lunes, martes, miércoles');
+}
