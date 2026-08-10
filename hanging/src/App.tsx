706268a6ce308ea07f7025351ba3c440f1f8eb49
Minami73 <jj.c.r.1781@gmail.com>
2026-08-09 22:04:09 -0600

    Proyecto del juego del ahorcado (React + Vite + TypeScript) en fase inicial

---
+++ hanging/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+    return(
+        <div className="App">
+            {/* Esto es un comentario de React */}
+            <h3>Imagen del juego</h3>
+
+            {/* Palabra oculta */}
+            <h3>_ _ _ _ _ _</h3>
+
+            {/* Contador de intentos */}
+            <h3>Intentos</h3>
+
+            {/* Botones de las letras */}
+            <button>A</button>
+            <button>B</button>
+            <button>C</button>
+        </div>
+    )
+}
+
+export default App
